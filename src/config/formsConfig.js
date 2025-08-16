99a1a9587aef3d6346366cb574c73ab410de537a
Bryan Muñoz <tiagotroller021019@gmail.com>
2025-08-15 23:42:34 -0500

    Actualizacion4

--- src/config/formsConfig.js
+++ src/config/formsConfig.js
@@ -4,229 +4,229 @@
 export const FORM_CONFIGS = [
   { 
     id: 1, 
-    name: 'Antidoto – 300 9741660', 
+    name: 'Antidoto', 
     url: 'https://forms.gle/7AdMT5JaHzjQBquK6',
     logo: 'ANTIDOTO.webp'
   },
   { 
     id: 2, 
-    name: 'Atiba – Cra 4 # 5-19', 
+    name: 'Atiba', 
     url: 'https://forms.gle/xTU3pNHe8hr7yirbA',
     logo: 'ATIBA.webp'
   },
   { 
     id: 3, 
-    name: 'Bocados – Cra 15 # 9-44 Las Lajas', 
+    name: 'Bocados', 
     url: 'https://forms.gle/yVYxK7Wndvi7nex17',
     logo: 'BOCADOS.webp'
   },
   { 
     id: 4, 
-    name: 'Bite & Dip – Calle 8 con Cra 14', 
+    name: 'Bite & Dip', 
     url: 'https://forms.gle/BpbQ4SAJXrhRK8UA',
     logo: 'BITE & DIP.webp'
   },
   { 
     id: 5, 
-    name: 'Burger Hunter – 301 9557383', 
+    name: 'Burger Hunter', 
     url: 'https://forms.gle/mGizB7ip2nycbQot9',
     logo: 'BURGER HUNTER.webp'
   },
   { 
     id: 6, 
-    name: 'Caoba – Calle 11 # 5-23 Santa Rita', 
+    name: 'Caoba', 
     url: 'https://forms.gle/mGizB7ip2nycbQot9',
     logo: 'CAOBA.webp'
   },
   { 
     id: 7, 
-    name: 'Conejo\'s Pizza – Calle 2 Este # 7-41', 
+    name: 'Conejo\'s Pizza', 
     url: 'https://forms.gle/Sg6kzHBCuCjz2Qi29',
     logo: 'CONEJOS PIZZA.webp'
   },
   { 
     id: 8, 
-    name: 'DaniBurgers – Calle 2 Este # 7-82 La Arboleda', 
+    name: 'DaniBurgers', 
     url: 'https://forms.gle/BKDpqtpYyqgsHB7G7',
     logo: 'DANI BURGERS.webp'
   },
   { 
     id: 9, 
-    name: 'De Gula – Calle 8 # 6-23', 
+    name: 'De Gula', 
     url: 'https://forms.gle/dkmwkqGySahXd23D6',
     logo: 'DE GULA.webp'
   },
   { 
     id: 10, 
-    name: 'Don Barriga – Cra 5 # 13-51', 
+    name: 'Don Barriga', 
     url: 'https://forms.gle/1Jy7JMUpHPBw9P488',
     logo: 'DON BARRIGA.webp'
   },
   { 
     id: 11, 
-    name: 'Dorilocos – Cra 4 # 8-27', 
+    name: 'Dorilocos', 
     url: 'https://forms.gle/1Yynzrby3kNb7u6y6',
     logo: 'DORILOCOS.webp'
   },
   { 
     id: 12, 
-    name: 'El Andino – Cra 5 # 11-19 Santa Rita', 
+    name: 'El Andino', 
     url: 'https://forms.gle/r5uB6nnUTSLF81W19',
     logo: 'EL ANDINO.webp'
   },
   { 
     id: 13, 
-    name: 'El Faro – Calle 8 # 4-84', 
+    name: 'El Faro', 
     url: 'https://forms.gle/cPJV8UTWLEBXp7dE9',
     logo: 'EL FARO.webp'
   },
   { 
     id: 14, 
-    name: 'Faca Food – Cra 5 # 15-145', 
+    name: 'Faca Food', 
     url: 'https://forms.gle/nNvMqhGi1WMD1qhe9',
     logo: 'FACA FOOD.webp'
   },
   { 
     id: 15, 
-    name: 'Fries – Cra 5 # 10-41 Santa Rita', 
+    name: 'Fries', 
     url: 'https://forms.gle/bHhoHg9DW3YKvB3DA',
     logo: 'FRIES.webp'
   },
   { 
     id: 16, 
-    name: 'Gustazo – Diag 148 # 52-198 Llano Popular', 
+    name: 'Gustazo', 
     url: 'https://forms.gle/hDEcxuZ3oPy1LCek7',
     logo: 'GUSTAZO.webp'
   },
   { 
     id: 17, 
-    name: 'J/S Pizzería – Calle 2A # 8-03', 
+    name: 'J/S Pizzería', 
     url: 'https://forms.gle/s8oVV93rysCa2pvN6',
     logo: 'JS PIZZERIA.webp'
   },
   { 
     id: 18, 
-    name: 'Jugoson – Cra 15 # 5A-06 Dos Caminos', 
+    name: 'Jugoson', 
     url: 'https://forms.gle/Rd5C5u7BquURFxzD6',
     logo: 'JUGOSON.webp'
   },
   { 
     id: 19, 
-    name: 'La Boca – Cra 5 # 10-41', 
+    name: 'La Boca', 
     url: 'https://forms.gle/zNxFHrrLuHNYwYps7',
     logo: 'LA BOCA.webp'
   },
   { 
     id: 20, 
-    name: 'La Roca Burger – Cra 7 # 9-91 Santa Rita', 
+    name: 'La Roca Burger', 
     url: 'https://forms.gle/QTDH6R1kcRXZxxgh7',
     logo: 'LA ROCA BURGER.webp'
   },
   { 
     id: 21, 
-    name: 'Mr Pizza – Cra 5 # 13-93 Omnicentro', 
+    name: 'Mr Pizza', 
     url: 'https://forms.gle/my5SFjmnegmaP9s99',
     logo: 'MR PIZZA.webp'
   },
   { 
     id: 22, 
-    name: 'Mr Toppings – Cra 7 # 5-40', 
+    name: 'Mr Toppings', 
     url: 'https://forms.gle/4JoKmbKU8aPJSHWCA',
     logo: 'MR TOPPINGS.webp'
   },
   { 
     id: 23, 
-    name: 'Oh My Dog – Calle 8 # 3A-65', 
+    name: 'Oh My Dog', 
     url: 'https://forms.gle/DkU9T71eV6usMY5NA',
     logo: 'OH MY DOG.webp'
   },
   { 
     id: 24, 
-    name: 'Oregon BBQ – 320 2409660', 
+    name: 'Oregon BBQ', 
     url: 'https://forms.gle/Jr3mdnbvgt9r4QMr9',
     logo: 'OREGON BBQ.webp'
   },
   { 
     id: 25, 
-    name: 'Papo Burger – Cra 5 # 4-12', 
+    name: 'Papo Burger', 
     url: 'https://forms.gle/7SqNHAXyEH97mW148',
     logo: 'PAPO BURGER.webp'
   },
   { 
     id: 26, 
-    name: 'Patatas House – Cra 4 # 8-23', 
+    name: 'Patatas House', 
     url: 'https://forms.gle/2d334Tn81jW7BPhdA',
     logo: 'PATATAS HOUSE.webp'
   },
   { 
     id: 27, 
-    name: 'Pecado Capital – Calle 11 # 5-07', 
+    name: 'Pecado Capital', 
     url: 'https://forms.gle/rZcMB1Le7xY2fj7H9',
     logo: 'PECADO CAPITAL.webp'
   },
   { 
     id: 28, 
-    name: 'Pedacito de Cielo – Cra 3 # 8-56', 
+    name: 'Pedacito de Cielo', 
     url: 'https://forms.gle/DGnBhktt9yATn2yN9',
     logo: 'PEDACITO DE CIELO.webp'
   },
   { 
     id: 29, 
-    name: 'Pilón Burguer – Calle 2A Este # 7-48', 
+    name: 'Pilón Burguer', 
     url: 'https://forms.gle/vdARuZyTWNPZUJpQA',
     logo: 'PILON BURGUER.webp'
   },
   { 
     id: 30, 
-    name: 'Punto Burguer – Calle 8 # 6-19', 
+    name: 'Punto Burgue', 
     url: 'https://forms.gle/o2dakUwnP7RfEUnA8',
     logo: 'PUNTO BURGUER.webp'
   },
   { 
     id: 31, 
-    name: 'Sabor Urbano – Cra 15 # 9-13', 
+    name: 'Sabor Urbano', 
     url: 'https://forms.gle/1dpKGnV2FmmJCD4J9',
     logo: 'SABOR URBANO.webp'
   },
   { 
     id: 32, 
-    name: 'Texas BBQ – Cra 4 # 7-167', 
+    name: 'Texas BBQ', 
     url: 'https://forms.gle/zWyXGqEdNjvmiVeW8',
     logo: 'TEXAS BBQ.webp'
   },
   { 
     id: 33, 
-    name: 'Viñas Food – Calle 11 # 8-03', 
+    name: 'Viñas Food', 
     url: 'https://forms.gle/5EmYjg7D2DvUfqMX8',
     logo: 'VINAS.webp'
   },
   { 
     id: 34, 
-    name: 'Yadah Food – Calle 12 # 6-42', 
+    name: 'Yadah Food', 
     url: 'https://forms.gle/SGvSu8onhroS9Dm9A',
     logo: 'YADAH FOOD.webp'
   },
   { 
     id: 35, 
-    name: 'Y&Y Burgers – Calle 8 # 7A-10', 
+    name: 'Y&Y Burgers', 
     url: 'https://forms.gle/1m52oJKa5TmMUdgo6',
     logo: 'Y&Y.webp'
   },
   { 
     id: 36, 
-    name: 'Yokai – Calle 15 # 4-59 Los Molinos Serai', 
+    name: 'Yokai', 
     url: 'https://forms.gle/QeSh31tFu7TAz5eZ9',
     logo: 'YOKAI.webp'
   },
   { 
     id: 37, 
-    name: 'Super Rico – Calle 10 # 3-25', 
+    name: 'Super Rico', 
     url: 'https://forms.gle/QeSh31tFu7TAz5eZ9',
     logo: 'SUPER RICO.webp'
   },
   { 
     id: 38, 
-    name: 'Bite & Dip Express – Cra 6 # 12-18', 
+    name: 'BURGER HOT', 
     url: 'https://forms.gle/KSpCWexTUXkYMfnTA',
     logo: 'BURGER HOT.webp'
   },
